680206f15c048d432a4d1c582845437295f6e1aa
Amber Widmer <amberplehman@g.cofc.edu>
2018-04-18 22:49:56 -0400

    adds predictions and related mvp selections to df

--- build_mvp_glm.R
+++ build_mvp_glm.R
@@ -8,8 +8,18 @@ topPlayers = mvpRanks %>%
   group_by(season_end) %>%
   top_n(5) %>%
   select(player, MVP, GS, PS.G, TRB, AST, STL, BLK, TOV, OBPM, DBPM) 
+#there are 6 values in 2007 for some reason
+topPlayers <- topPlayers[-91,]
 
+#add predictions to df and classify mvp
 mvp_glm <- glm(MVP ~ GS + PS.G + TRB + AST + STL + BLK + TOV + OBPM + DBPM, data = topPlayers, family = binomial)
+topPlayers$mvp_glm <- predict(mvp_glm, topPlayers[, c('GS', 'PS.G', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'OBPM', 'DBPM')], type="response")
+for (i in seq(1,nrow(topPlayers),5)){
+  for (j in 0:4){
+  topPlayers[i+j,14] <- ifelse(topPlayers[i+j,13]==max(topPlayers[i:(i+4),13]),
+                                 1,0)
+  }
+}
 
 topPlayersTest = mvpRanks %>%
   filter((PS.G >= 20 & OBPM >= 0 & DBPM >= 0) | MVP == 1,  season_end > 2012) %>%
@@ -19,6 +29,7 @@ topPlayersTest = mvpRanks %>%
 
 # logistic regression
 mvp_glm.test = predict(mvp_glm, topPlayersTest[, c('GS', 'PS.G', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'OBPM', 'DBPM')], type="response")
+topPlayersTest$mvp_glm_test <- mvp_glm.test
 
 topPlayersTest
 mvp_glm.test
